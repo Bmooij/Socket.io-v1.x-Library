75c51b298dc2b135ff08821d46e8e501d8c3269e
Bob Mooij <b.mooij@beeliners.com>
2019-02-24 23:00:53 +0100

    Update example

--- examples/HelloTime_ESP32.ino
+++ examples/HelloTime_ESP32.ino
@@ -6,17 +6,15 @@
  *
  */
 #define ESP32
+#define DEBUG_WEBSOCKETS
 #include <SocketIOClient.h>
 
 SocketIOClient client;
 const char* ssid     = "xxxxxxxxxxxxxxxxx";
 const char* password = "xxxxxxxxxxxxx";
 
-char host[] = "192.168.1.1";
-int port = 1234;
-extern String RID;
-extern String Rname;
-extern String Rcontent;
+char host[] = "192.168.178.26";
+int port = 3484;
 
 unsigned long previousMillis = 0;
 long interval = 5000;
@@ -24,61 +22,50 @@ unsigned long lastreply = 0;
 unsigned long lastsend = 0;
 
 void setup() {
-  Serial.begin(115200);
-  delay(10);
+    Serial.begin(9600);
+    Serial.println();
+    delay(10);
 
-  // We start by connecting to a WiFi network
+    // We start by connecting to a WiFi network
 
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  
-  WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
 
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+    WiFi.begin(ssid, password);
 
-  if (!client.connect(host, port)) {
-    Serial.println("connection failed");
-    return;
-  }
-if (client.connected())
-  {
-    client.send("connection", "message", "Connected !!!!");
-  }
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    client.on("rtime", [](const String &data, ackCallback_fn cb){
+        Serial.print("Il est ");
+        Serial.println(data);
+        cb("confirmation for received!");
+    });
+
+    if (!client.connect(host, port)) {
+        Serial.println("connection failed");
+        return;
+    }
+    if (client.connected()) {
+        client.send("Connected !!!!");
+    }
 }
 
 void loop() {
-unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis > interval)
-  {
-    previousMillis = currentMillis;
-    //client.heartbeat(0);
-    Serial.print("**************************************  ");
-    Serial.println(lastsend);
-    client.send("atime", "message", "Time please?");
-    lastsend = millis();
-    Serial.print("**************************************  ");
-    Serial.println(lastsend);
-  }
-  if (client.monitor())
-  {
-    lastreply = millis(); 
-    Serial.println(RID);
-    if (RID == "atime" && Rname == "time")
-    {
-      Serial.print("Il est ");
-      Serial.println(Rcontent);
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis > interval) {
+        previousMillis = currentMillis;
+        client.emit("atime", "{\"message\":\"Time please?\"}");
     }
-  }
-  
-
+    client.monitor();
 }
+
